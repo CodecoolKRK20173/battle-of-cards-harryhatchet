7d177fdd7a20abee4af23642e4dddc52146c4be3
johnny-sack1 <jonymaka@gmail.com>
2018-04-09 17:30:39 +0200

    Create Deck class with constructor

---
+++ src/com/codecool/poker/Deck.java
@@ -0,0 +1,16 @@
+package com.codecool.poker;
+
+import java.util.*;
+
+public class Deck {
+
+    private List<Card> deck = new ArrayList<>();
+
+    public Deck() {
+        for (Suit suit: Suit.values()) {
+            for (Rank rank: Rank.values()) {
+                this.deck.add(new Card(rank, suit));
+            }
+        }
+    }
+}
