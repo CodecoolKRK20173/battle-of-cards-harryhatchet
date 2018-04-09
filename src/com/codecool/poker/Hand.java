b30a7ffa8ac748ef98db50bd6ad1272d71fe5763
Elzacodecool <elliegolec@gmail.com>
2018-04-09 23:18:26 +0200

    Implement sort cards

--- src/com/codecool/poker/Hand.java
+++ src/com/codecool/poker/Hand.java
@@ -2,6 +2,8 @@ package com.codecool.poker;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Hand {
     private List<Card> cards;
@@ -10,6 +12,7 @@ public class Hand {
     public Hand(List<Card> cards) {
         this.cards = cards;
         this.handPoints = 100;
+        sortCards();
     }
 
     public void addCard(Card card) {
@@ -23,6 +26,52 @@ public class Hand {
 
     public List<Card> getCards() {
         return this.cards;
+    }
+
+    private Map<Integer, Integer> calculateCardsOccurrence() {
+        Map<Integer, Integer> cardsOccurrence = new HashMap<>();
+        Integer count;
+        Integer strength;
+        for(Card card: cards) {
+            strength = card.getRank().getCardStrength();
+            if(cardsOccurrence.containsKey(strength)) {
+                count = cardsOccurrence.get(strength) + 1;
+            } else {
+                count = 1;
+            }
+            cardsOccurrence.put(strength, count);
+        }
+        return cardsOccurrence;
+    }
+
+    private void sortCards() {
+        Map<Integer, Integer> cardsOccurrence = calculateCardsOccurrence();
+        List<Card> sortedCards = new ArrayList<>();
+        Integer count1;
+        Integer count2;
+        
+        for(Card card: cards) {
+            count1 = card.getRank().getCardStrength();
+
+            for(int i = 0; i < sortedCards.size(); i++) {
+                count2 = sortedCards.get(i).getRank().getCardStrength();
+
+                if(cardsOccurrence.get(count1) > (cardsOccurrence.get(count2))) {
+                    sortedCards.add(i, card);
+                    break;
+                } else if (cardsOccurrence.get(count1) == (cardsOccurrence.get(count2))) {
+                    if(count1 > count2) {
+                        sortedCards.add(i, card);
+                        break;
+                    }
+                } 
+            }
+            if (!sortedCards.contains(card)) {
+                sortedCards.add(card);
+            }  
+        }
+        
+        this.cards = sortedCards;
     }
 
     public static void main(String[] args) {
