c566490b1a9939ccd8db63c8f8f354cc41ac31a6
Elzacodecool <elliegolec@gmail.com>
2018-04-09 20:43:44 +0200

    Add constructor

--- src/com/codecool/poker/Hand.java
+++ src/com/codecool/poker/Hand.java
@@ -1,6 +1,13 @@
 package com.codecool.poker;
 
 public class Hand {
+    List<Card> cards;
+    int handPoints;
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+        this.handPoints = 100;
+    }
 
     public static void main(String[] args) {
         System.out.println("Test main");
