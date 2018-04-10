3c7dfd76cae32e40d16037532f636b8aad60226c
Senseinvader <d.ivantsov1983@gmail.com>
2018-04-10 15:31:58 +0200

    update makeaRaise and create countCardsToRemove in makeRaise add check the raise amount add array of cardStatus for cards to be changed

--- src/com/codecool/poker/HumanPlayer.java
+++ src/com/codecool/poker/HumanPlayer.java
@@ -1,9 +1,13 @@
 package com.codecool.poker;
 
+import java.util.List;
+import java.util.Scanner;
+
 
 public class HumanPlayer extends Player {
 
-    Hand hand = new Hand();
+    Hand hand = new Hand(cards);
+    Scanner sc = new Scanner(System.in);
 
     private int chips=100;
     private int bet=0;
@@ -12,6 +16,7 @@ public class HumanPlayer extends Player {
     private boolean isSmallBlind = false;
     private boolean isBigBlind = false;
     private boolean isUTG = false;
+    int minRaise;
 
     public void addChips(int newChips) {
         chips += newChips;
@@ -48,23 +53,46 @@ public class HumanPlayer extends Player {
         }
     }
 
-    private int makeRaise (int playersRaise) {
-        if (table.getDiff() == 1) {
-            minRaise = table.getMaxBet() + 2;
-        } else {
-            minRaise = table.getMaxBet() + table.getDiff();
+    public int removeCards() {
+        boolean[] cardStatus = {false, false, false, false, false};
+        int cardToDissmiss;
+        do {
+            System.out.println("Which cards do you wish to change?");
+            cardToDissmiss = sc.nextInt();
+            cardStatus[cardToDissmiss-1] = true;
+        } while (cardToDissmiss == 0);
+
+
+        int discard = 0;
+        for (int i=4; i >= 0; i--) {
+            if (cardStatus[i] == true) {
+                discard++;
+            }
         }
-        bet = minRaise + playersRaise;
-        if (bet > getChips()) {
-            bet = getChips();
-        }
-        throwChips(bet);
-        return bet;
+        return discard;
     }
-    
+
+
+    private int makeRaise (int playersRaise) {
+        while (playersRaise > getChips()) {
+            System.out.println("Please choose avaliable size of bet");
+            playersRaise = sc.nextInt();
+        }
+            if (table.getDiff() == 1) {
+                minRaise = table.getMaxBet() + 2;
+            } else {
+                minRaise = table.getMaxBet() + table.getDiff();
+            }
+            bet = minRaise + playersRaise;
+            if (bet > getChips()) {
+                bet = getChips();
+            }
+            throwChips(bet);
+            return bet;
+    }    
 
     private int makeCall() {
-        bet = getMaxBet();
+        bet = table.getMaxBet();
         throwChips(bet);
         return bet;
         }
