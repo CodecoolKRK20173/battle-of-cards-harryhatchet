5351bb7475ffd3a4dd760e14171d026b01ff96b1
WojTechM <wojciech.m1997@gmail.com>
2018-04-10 09:59:03 +0200

    Create sample version of Table class

---
+++ Table.java
@@ -0,0 +1,22 @@
+package com.codecool.poker;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Table {
+
+    private final int NUM_OF_PLAYERS = 4;
+    private List<Player> players;
+    private Deck deck;
+
+    public Table() {
+        initPlayers();
+        this.deck = new Deck(); 
+    }
+
+    private initPlayers() {
+        for (int i = 0; i < NUM_OF_PLAYERS; i++) {
+            players.add(new HumanPlayer());
+        }
+    }
+}
