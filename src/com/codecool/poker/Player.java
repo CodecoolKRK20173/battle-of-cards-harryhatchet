d36da5e2cd928d5e2b508036aaf096e83160222d
Senseinvader <d.ivantsov1983@gmail.com>
2018-04-10 09:47:23 +0200

    change list of abstract methods

--- src/com/codecool/poker/Player.java
+++ src/com/codecool/poker/Player.java
@@ -6,12 +6,13 @@ public abstract class Player {
 
     public abstract boolean getFold();
     public abstract int getChips();
-    public abstract void setChips(int number);
     public abstract int getBet();
     public abstract Hand getHand();
     public abstract void takeAction();
     public abstract boolean placeBet();
-    public abstract void unFold();
+    public abstract void fold();
     public abstract void switchDealer();
+    public abstract void switchSmallBlind();
+    public abstract void switchBigBlind();
 
 }
