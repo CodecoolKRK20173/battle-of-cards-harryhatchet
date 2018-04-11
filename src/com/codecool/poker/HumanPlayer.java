ab5e27d439c710915421d590d1429ce45032fe6f
Senseinvader <d.ivantsov1983@gmail.com>
2018-04-11 14:09:41 +0200

    rearrange methods in class and create class constructor

--- src/com/codecool/poker/HumanPlayer.java
+++ src/com/codecool/poker/HumanPlayer.java
@@ -6,25 +6,17 @@ import java.util.Scanner;
 
 public class HumanPlayer extends Player {
 
+    public HumanPlayer() {
+        this.table = table;
+    }
+
     Hand hand = new Hand(cards);
     Scanner sc = new Scanner(System.in);
 
     private int chips=100;
     private int bet=0;
     private boolean isFold = false;
-    private boolean isDealer = false;
-    private boolean isSmallBlind = false;
-    private boolean isBigBlind = false;
-    private boolean isUTG = false;
     private int minRaise;
-
-    public void addChips(int newChips) {
-        chips += newChips;
-    }
-
-    public void throwChips(int bet) {
-        chips -= bet;
-    }
 
     public int getChips() {
         return chips;
@@ -36,76 +28,6 @@ public class HumanPlayer extends Player {
 
     public Hand getHand() {
         return hand;
-    }
-
-    // public int placeBet(int customBet) {
-    //     if (isSmallBlind) {
-
-    // }
-
-    public void resetBet() {
-        this.bet = 0;
-    }
-
-    public void resetFold() {
-        if (isFold == true && getChips() > 0) {
-            isFold = false;
-        }
-    }
-
-    public int changeCards() {
-        boolean[] cardStatus = {false, false, false, false, false};
-        int cardToDissmiss;
-        do {
-            System.out.println("Choose cards to change or press 0 to quit");
-            cardToDissmiss = sc.nextInt();
-            cardStatus[cardToDissmiss-1] = true;
-        } while (cardToDissmiss == 0);
-
-        int discard = 0;
-        for (int i=4; i >= 0; i--) {
-            if (cardStatus[i]) {
-                hand.discard(i);
-                discard++;
-            }
-        }
-        return discard;
-    }
-
-    int makeRaise (int playersRaise) {
-        minRaise = table.getMaxBet() + table.getDiff();
-        
-        while ((minRaise + playersRaise) > getChips()) {                             // Excessive bet control
-            System.out.println("Please choose avaliable size of bet");
-            playersRaise = sc.nextInt();
-        }
-        bet = minRaise + playersRaise;
-        throwChips(bet);
-        return bet;
-    }    
-
-    int makeCall() {
-        if ((table.getMaxBet() - getBet()) > getChips() && getChips() > 0) {
-            bet = throwChips(getChips());
-        } else {
-            bet = throwChips(table.getMaxBet() - getBet());
-        }
-        return bet;
-    }
-
-    int makeCheck(){
-        if (table.getMaxBet() == getBet()) {
-            return bet;
-        }
-        return null;
-    }
-
-    public int getBet() {
-        return bet;
-    }
-
-    public void fold() {
-        isFold = !isFold;
     }
 
     public void setDealer() {
@@ -140,4 +62,76 @@ public class HumanPlayer extends Player {
         return position.equals(Position.UTG);
     }
 
+    public void addChips(int newChips) {
+        chips += newChips;
+    }
+
+    private void throwChips(int bet) {
+        chips -= bet;
+    }
+
+    public void resetBet() {
+        this.bet = 0;
+    }
+
+    public void resetFold() {
+        if (isFold == true && getChips() > 0) {
+            isFold = false;
+        }
+    }
+
+    public int changeCards() {
+        boolean[] cardStatus = {false, false, false, false, false};
+        int cardToDissmiss;
+        do {
+            System.out.println("Choose cards to change or press 0 to quit");
+            cardToDissmiss = sc.nextInt();
+            cardStatus[cardToDissmiss-1] = true;
+        } while (cardToDissmiss == 0);
+
+        int discard = 0;
+        for (int i=4; i >= 0; i--) {
+            if (cardStatus[i]) {
+                hand.discard(i);
+                discard++;
+            }
+        }
+        return discard;
+    }
+
+    public int makeRaise (int playersRaise) {
+        minRaise = table.getMaxBet() + table.getDiff();
+        
+        while ((minRaise + playersRaise) > getChips()) {                             // Excessive bet control
+            System.out.println("Please choose avaliable size of bet");
+            playersRaise = sc.nextInt();
+        }
+        bet = minRaise + playersRaise;
+        throwChips(bet);
+        return bet;
+    }    
+
+    public int makeCall() {
+        if ((table.getMaxBet() - getBet()) > getChips() && getChips() > 0) {
+            bet = throwChips(getChips());
+        } else {
+            bet = throwChips(table.getMaxBet() - getBet());
+        }
+        return bet;
+    }
+
+    public int makeCheck(){
+        if (table.getMaxBet() == getBet()) {
+            return bet;
+        }
+        return null;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    private void fold() {
+        isFold = !isFold;
+    }
 }
