cc27e45db863e97421310660ab1a80e53f060bff
Elzacodecool <elliegolec@gmail.com>
2018-04-11 15:32:48 +0200

    Implement find the best hand

--- src/com/codecool/poker/HandComparator.java
+++ src/com/codecool/poker/HandComparator.java
@@ -7,9 +7,39 @@ import java.util.HashMap;
 
 public class HandComparator {
     List<Hand> hands;
+    List<Hand> theBestHand;
 
     public HandComparator(List<Hand> hands) {
         this.hands = hands;
+        this.theBestHand = findTheBestHand();
     }
-    
+
+    public List<Hand> getTheBestHand() {
+        return this.theBestHand;
+    }
+
+    private List<Hand> findTheBestHand() {
+        List<Hand> theBestHand = new ArrayList<>();
+        Hand maxHand = hands.get(0);
+
+        for(Hand hand: hands) {
+            if(hand.getHandPoints() > maxHand.getHandPoints()) {
+                theBestHand.clear();
+                theBestHand.add(hand);
+                maxHand = hand;
+            } else if(hand.getHandPoints() == maxHand.getHandPoints()) {
+                
+                if(hand.compareTo(maxHand) > 0) {
+                    theBestHand.clear();
+                    theBestHand.add(hand);
+                    maxHand = hand;
+                } else if(hand.compareTo(maxHand) == 0) {
+                    theBestHand.add(hand);
+                }
+                
+            }
+        }
+
+        return theBestHand;
+    }
 }
