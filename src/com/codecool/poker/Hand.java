1d93e3e57752d8bc243744cfde20085b9a7880ea
Elzacodecool <elliegolec@gmail.com>
2018-04-11 13:17:33 +0200

    Add compareTo and equals method

--- src/com/codecool/poker/Hand.java
+++ src/com/codecool/poker/Hand.java
@@ -6,8 +6,9 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.lang.Comparable;
 
-public class Hand {
+public class Hand implements Comparable<Hand> {
     private List<Card> cards;
     private Map<Integer, Integer> cardsOccurrence;
     private HandPoints handPoints;
@@ -32,14 +33,14 @@ public class Hand {
     }
 
     public void replaceCards(List<Card> cardsToDelete, List<Card> cardsToAdd) {
-        for(int i = 0; i < cardsToDelete.size(); i++) {
+        for (int i = 0; i < cardsToDelete.size(); i++) {
             this.cards.remove(cardsToDelete.get(i));
             this.cards.add(cardsToAdd.get(i));
         }
     }
 
     public void addCards(List<Card> cardsToAdd) {
-        for(int i = 0; i < cardsToAdd.size(); i++) {
+        for (int i = 0; i < cardsToAdd.size(); i++) {
             this.cards.add(cardsToAdd.get(i));
         }
     }
@@ -52,9 +53,9 @@ public class Hand {
         Map<Integer, Integer> cardsOccurrence = new HashMap<>();
         Integer count;
         Integer strength;
-        for(Card card: cards) {
+        for (Card card : cards) {
             strength = card.getRank().getCardStrength();
-            if(cardsOccurrence.containsKey(strength)) {
+            if (cardsOccurrence.containsKey(strength)) {
                 count = cardsOccurrence.get(strength) + 1;
             } else {
                 count = 1;
@@ -68,28 +69,37 @@ public class Hand {
         List<Card> sortedCards = new ArrayList<>();
         Integer count1;
         Integer count2;
-        
-        for(Card card: cards) {
+
+        for (Card card : cards) {
             count1 = card.getRank().getCardStrength();
 
-            for(int i = 0; i < sortedCards.size(); i++) {
+            for (int i = 0; i < sortedCards.size(); i++) {
                 count2 = sortedCards.get(i).getRank().getCardStrength();
 
-                if(cardsOccurrence.get(count1) > (cardsOccurrence.get(count2))) {
+                if (cardsOccurrence.get(count1) > (cardsOccurrence.get(count2))) {
                     sortedCards.add(i, card);
                     break;
                 } else if (cardsOccurrence.get(count1) == (cardsOccurrence.get(count2))) {
-                    if(count1 > count2) {
+                    if (count1 > count2) {
                         sortedCards.add(i, card);
                         break;
                     }
-                } 
+                }
             }
             if (!sortedCards.contains(card)) {
                 sortedCards.add(card);
-            }  
+            }
         }
-        
+
         this.cards = sortedCards;
+    }
+
+    @Override
+    public int compareTo(Hand otherHand) {
+
+    }
+    
+    private boolean equals(Hand otherHand) {
+
     }
 }
