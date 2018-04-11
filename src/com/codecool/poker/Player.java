4779b8cb75277cd250d4a6be25ef325451edc845
Senseinvader <d.ivantsov1983@gmail.com>
2018-04-11 10:14:44 +0200

    refactor Player class, HumanPlayer class add missinig method signatures to abstract class add deleting cards from hand during changeCards change access modifiers to methods in HumanPlayer

--- src/com/codecool/poker/Player.java
+++ src/com/codecool/poker/Player.java
@@ -4,22 +4,35 @@ package com.codecool.poker;
 public abstract class Player {
 
     public abstract boolean getFold();
+
     public abstract int getChips();
     public abstract void addChips(int newChips);
+    public abstract void throwChips(int bet);
+
     public abstract Hand getHand();
     public abstract void setHand(Hand hand);
+
     public abstract void resetBet();
     public abstract void resetFold();
+
     public abstract int getBet();
-    public abstract boolean placeBet();
+    public abstract int makeRaise();
+    public abstract int makeCall();
+
     public abstract void fold();
+
     public abstract void setDealer();
     public abstract void setSmallBlind();
     public abstract void setBigBlind();
     public abstract void setUTG();
+
     public abstract boolean getCurrentDealer();
     public abstract boolean getCurrentSmallBlind();
     public abstract boolean getCurrentBigBlind();
     public abstract boolean getCurrentUTG();
+
+    public enum playerStatus {
+        DEALER, SMALL_BLIND, BIG_BLIND, UTG;
+    }
 
 }
