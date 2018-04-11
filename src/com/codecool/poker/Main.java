cf9abb84dae5ca52b7df79a1fa6ee52ced2b5d01
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-04-11 11:33:18 +0200

    Changes for FX

--- src/com/codecool/poker/Main.java
+++ src/com/codecool/poker/Main.java
@@ -9,11 +9,11 @@ public class Main {
         Deck deck = new Deck();
         deck.shuffle();
         List<Card> drawnCards = new ArrayList<>();
-        Hand hand = new Hand(deck.drawCards(5));
+        /*Hand hand = new Hand(deck.drawCards(5));
         
 
         for (Card card : hand.getCards()) {
             System.out.println(card.getRank().toString() + card.getSuit().toString());
-        }
+        }*/
     }
 }
