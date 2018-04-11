d540869d05f239b04897d93eadf9a2c82e4d9df0
Elzacodecool <elliegolec@gmail.com>
2018-04-11 11:25:59 +0200

    Test

--- src/com/codecool/poker/Main.java
+++ src/com/codecool/poker/Main.java
@@ -9,15 +9,17 @@ public class Main {
        
         List<Card> drawnCards = new ArrayList<>();
         Hand hand;
+        int i = 0;
         do { 
             Deck deck = new Deck();
             hand = new Hand(deck.drawCards(5));
-
+            i++;
             
         } while (hand.getHandPoints() != 10 );
         for (Card card : hand.getCards()) {
             System.out.println(card.getRank().toString() + card.getSuit().toString());
         }
         System.out.println(hand.getHandPoints());
+        System.out.println(i);
     }
 }
