9ff845435112e23aba1f63c457cee1390ecbf1a3
Elzacodecool <elliegolec@gmail.com>
2018-04-11 15:00:21 +0200

    test compareTo methods

--- src/com/codecool/poker/Main.java
+++ src/com/codecool/poker/Main.java
@@ -8,18 +8,21 @@ public class Main {
 
        
         List<Card> drawnCards = new ArrayList<>();
-        Hand hand;
-        int i = 0;
-        do { 
-            Deck deck = new Deck();
-            hand = new Hand(deck.drawCards(5));
-            i++;
-            
-        } while (hand.getHandPoints() != 10 );
-        for (Card card : hand.getCards()) {
+        Hand hand1;
+        Hand hand2;
+        Deck deck = new Deck();
+
+    
+        hand1 = new Hand(deck.drawCards(5));
+        hand2 = new Hand(deck.drawCards(5));
+        
+        for (Card card : hand1.getCards()) {
             System.out.println(card.getRank().toString() + card.getSuit().toString());
         }
-        System.out.println(hand.getHandPoints());
-        System.out.println(i);
+        System.out.println();
+        for (Card card : hand2.getCards()) {
+            System.out.println(card.getRank().toString() + card.getSuit().toString());
+        }
+        System.out.println(hand1.compareTo(hand2));
     }
 }
